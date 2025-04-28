f39f0c0e840a8699b62b6cc14018618e40611faf
EHronek <emmanuelhronek@gmail.com>
2025-04-28 20:36:04 +0300

    duck typing

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,12 +1,13 @@
-from typing import Mapping, Any, TypeVar, Union
+#!/usr/bin/env python3
 """Create a type variable that can represent any type"""
+from typing import Mapping, Any, TypeVar, Union
 
 T = TypeVar('T')
 
 
 def safely_get_value(
-    dct: Mapping[Any, Any], 
-    key: Any, 
+    dct: Mapping[Any, Any],
+    key: Any,
     default: Union[T, None] = None
 ) -> Union[Any, T]:
     if key in dct:
