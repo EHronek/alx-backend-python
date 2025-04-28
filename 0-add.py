b13110c2e098a047a916d167a688a63d7f2acdb8
EHronek <emmanuelhronek@gmail.com>
2025-04-28 16:59:09 +0300

    First task

--- 0-add.py
+++ 0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Defines a type-annotated function add """
 
+
 def add(a: float, b: float) -> float:
-   """Annotatted function add"""
-   return (a + b)
+    """Annotatted function add"""
+    return (a + b)
